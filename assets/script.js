8fe783fa93ffad27999b7f72a650a2f2b1eae17b
Craig Leslie <cleslie25@gmail.com>
2022-03-15 00:19:32 -0500

    Dynamically display info
    Changed the weather information to be displayed dynamically using
    moment js for the current date.
    Wrote a TODO list in pseudo code with a plan to make the site
    more dynamic.

--- assets/script.js
+++ assets/script.js
@@ -6,10 +6,29 @@ var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&ap
 
 
 
+// Dynamically creates weather information for current day
 
-var weatherDetails = document.getElementById('weatherAttributes')
+var date = moment().format('l')
+console.log (date)
 
+var main = document.getElementById('mainDiv')
+main.classList.add('container', 'col-9', 'outline')
 
+var title = document.createElement('h3')
+title.textContent = `${city} ${date}`
+title.classList.add('h3')
+
+var weatherDetails = document.createElement('ul')
+
+main.appendChild(title)
+main.appendChild(weatherDetails)
+
+//TODO create cards dynamically
+
+//moment js variables for each five days in future
+//create one card and test
+//create all five in a for loop
+//read api docs and see how to access future weather
 
 
 
@@ -22,29 +41,35 @@ fetch(queryURL)
     console.log(data)
     console.log(data.wind.speed)
 
+
     var temp = data.main.temp
     var listItemTemp = document.createElement('li')
     listItemTemp.textContent = `Current Temperature: ${temp}`
+    listItemTemp.classList.add('li')
     weatherDetails.appendChild(listItemTemp)
 
     var tempLow = data.main.temp_min    
     var listItemLo = document.createElement('li')
     listItemLo.textContent = `24hr Low: ${tempLow}`
+    listItemLo.classList.add('li')
     weatherDetails.appendChild(listItemLo)
 
     var tempHigh = data.main.temp_max
     var listItemHigh = document.createElement('li')
     listItemHigh.textContent = `24hr High: ${tempHigh}`
+    listItemHigh.classList.add('li')
     weatherDetails.appendChild(listItemHigh)
     
     var windSpeed = data.wind.speed
     var listItemWind = document.createElement('li')
     listItemWind.textContent = `Wind speed: ${windSpeed} MPH`
+    listItemWind.classList.add('li')
     weatherDetails.appendChild(listItemWind)
 
     var humidity = data.main.humidity
     var listItemHumid = document.createElement('li')
     listItemHumid.textContent = `Humidity: ${humidity}%`
+    listItemHumid.classList.add('li')
     weatherDetails.appendChild(listItemHumid)
   });
 
